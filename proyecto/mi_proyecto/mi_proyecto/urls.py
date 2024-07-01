b1b076f9b8118121cb782c1110884110edd614ae
nicoxx541 <134658242+nicoxx541@users.noreply.github.com>
2024-06-30 21:49:41 -0400

    Delete proyecto/mi_proyecto directory

--- proyecto/mi_proyecto/mi_proyecto/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mi_aplicacion.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
